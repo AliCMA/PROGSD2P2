7e5e461db37e13dcc43e7ba64c21c7aac1936c47
Ali <90691339+AliCMA@users.noreply.github.com>
2022-11-24 14:30:30 +0100

    Week-1 opdracht-3

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+include "./private/voorbeeldinclude.php";
+echo "Included";
+
+include "./private/voorbeeldrequired.php";
+echo "Required";
+
+include "./private/header.php";
+echo "Header";
+
+include "./private/footer.php";
+echo "Footer";
+
+
+
+
+
