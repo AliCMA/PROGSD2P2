ca5cc20d5dd69dbc8f94a55151022beb7386bcb7
Ali <90691339+AliCMA@users.noreply.github.com>
2022-12-01 12:28:00 +0100

    Week 2. Opdracht 1. CMS

---
+++ public/contentbeheer/startpagina.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Startpagina</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sono:wght@600&display=swap" rel="stylesheet"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap" rel="stylesheet"> 
+</head>
+<body>
+
+<header>
+  <b>
+
+<?php 
+include "./private/shared/header.php";
+?>
+
+</b>
+</header>
+
+
+<section>
+
+<div>
+  
+<h1 class="startpagina">
+  Hier kan je tekst invoegen, Hier kan je tekst invoegen, Hier kan je tekst invoegen, Hier kan je tekst invoegen, Hier kan je tekst invoegen, Hier kan je tekst invoegen.
+  Hier kan je tekst invoegen, Hier kan je tekst invoegen, Hier kan je tekst invoegen, Hier kan je tekst invoegen, Hier kan je tekst invoegen.
+  Hier kan je tekst invoegen, Hier kan je tekst invoegen, Hier kan je tekst invoegen, Hier kan je tekst invoegen, Hier kan je tekst invoegen.
+</h1>
+
+</div>
+
+</section>
+
+
+<footer class="footer">
+  <b>
+
+<?php 
+include "./private/shared/footer.php";
+?>
+
+</b>
+</footer>
+
+</body>
+</html>
