960e0f70bb4eafc53fc2eea34884ee2b87c1a553
Ali <90691339+AliCMA@users.noreply.github.com>
2022-12-01 12:57:37 +0100

    Week-2-Opdracht-2 (Laatste stap volgende week -Simon)

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+include "./private/shared/voorbeeldinclude.php";
+echo "Included";
+
+include "./private/shared/voorbeeldrequired.php";
+echo "Required";
+
+include "./private/shared/header.php";
+echo "Header";
+
+include "./private/shared/footer.php";
+echo "Footer";
+
+?> 
