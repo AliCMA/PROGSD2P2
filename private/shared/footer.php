2b7d512c3cd59d4fa8f9ec188e7cd0030861b9b3
Ali <90691339+AliCMA@users.noreply.github.com>
2022-12-08 12:09:12 +0100

    Week-2-opdracht-4

---
+++ private/shared/footer.php
@@ -0,0 +1,34 @@
+
+<?php 
+ 
+//  include "../init.php";
+ 
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" href= <?php echo url_path("/css/style.css") ?>>
+    <link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
+    
+
+
+<footer>
+   Dit is de footer
+</footer>
+
+
+<?php
+
+
+?>
+
+
+</body>
+</html>
